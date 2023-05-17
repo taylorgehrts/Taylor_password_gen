8f66d266b2815412751f01e6343e6ddfa7ac0e32
taylorgehrts <taylorgehrts@gmail.com>
2023-05-17 11:54:51 -0700

    confirmed button press

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 // Assignment code here
+var lowercaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var uppercaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var specialChars = ['!', '@', '#', '$', '%', '^', '&', '*', '()'];
 
+
+//main funtion//
+function generatePassword() {
+console.log ("this is where password will live")
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
